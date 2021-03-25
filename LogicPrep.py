5dae2ad770a1f07051f77e1c39767ae73ea6c3c0
astroboi-lab <terry007x@korea.ac.kr>
2021-03-25 17:17:17 +0900

    make_df_to_dict in LogicPrep.py

--- LogicPrep.py
+++ LogicPrep.py
@@ -1,4 +1,14 @@
 
 from astroboi_bio_tools.ToolLogicPrep import ToolLogicPreps
 class LogicPreps(ToolLogicPreps):
-    pass
+    def make_df_to_dict(self, df, key_idx, val_idx):
+        result_dict = {}
+        len_df = len(df)
+        for i in range(len_df):
+            key = df.loc[i][key_idx]
+            val = df.loc[i][val_idx]
+            if key in result_dict:
+                print(key, "is NOT unique!!!!")
+            else:
+                result_dict.update({key: val})
+        return result_dict
